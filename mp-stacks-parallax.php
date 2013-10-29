ad4266b6926bc66ad77bf7cc30da8cbff171761a
Phil Johnston <johnstonphilip@gmail.com>
2013-10-28 20:29:58 -0400

    beta1.0.0.6
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- mp-stacks-parallax.php
+++ mp-stacks-parallax.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: MP Stacks Parallax Addon
+Plugin Name: MP Stacks Parallax
 Plugin URI: http://moveplugins.com
 Description: This is an addon to the mp_stacks plugin which makes your stacks scroll using a parallax effect
-Version: beta1.0.0.5
+Version: beta1.0.0.6
 Author: Move Plugins
 Author URI: http://moveplugins.com
 Text Domain: mp_stacks_parallax
