153452d45204bcff99510c767f1a6511e7424c31
Phil Johnston <johnstonphilip@gmail.com>
2014-05-03 16:33:45 -0400

    beta1.0.1.8
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/misc-functions/parallax.php
+++ includes/misc-functions/parallax.php
@@ -25,13 +25,13 @@
 function mp_stacks_parallax_scripts(){
 		
 	//Scripts
-	wp_enqueue_script( 'mp_stacks_parallax_scripts', plugins_url( '/js/scripts.js', dirname( __FILE__ ) ), array( 'jquery' ) );
+	wp_enqueue_script( 'mp_stacks_parallax_scripts', plugins_url( '/js/scripts.js', dirname( __FILE__ ) ), array( 'jquery' ), false, true );
 	
 	//css
 	wp_enqueue_style( 'mp_stacks_parallax_css', plugins_url( '/css/style.css', dirname( __FILE__ ) ), array( 'mp_stacks_style' ) );
 	
 }
-add_action( 'mp_stacks_enqueue_scripts', 'mp_stacks_parallax_scripts');
+add_action( 'wp_enqueue_scripts', 'mp_stacks_parallax_scripts');
 
 /**
  * Filter Function which returns class name for a brick
